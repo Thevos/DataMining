27895fa9aaba2d6e795ac591954854f2496f03a4
noracato <ncschinkel@gmail.com>
2018-10-29 11:09:02 +0100

    paths

--- 2.Spam/spamcheck.r
+++ 2.Spam/spamcheck.r
@@ -1,3 +1,12 @@
 library(tm)
-txt <- system.file("data/fold1", "txt", package = "tm")
-reviews.neg <- Corpus(DirSource(txt, encoding="UTF-8"))
+txt.neg <- system.file("train/neg", "txt", package = "tm")
+txt.pos <- system.file("train/pos", "txt", package = "tm")
+reviews.neg <- Corpus(DirSource(txt.neg,
+                        encoding="UTF-8"))
+> reviews.pos <- Corpus(DirSource(txt.pos,
+                        encoding="UTF-8"))
+# Join negative and positive reviews into a single Corpus
+> reviews.all <- c(reviews.neg,reviews.pos)
+# create label vector (0=negative, 1=positive)
+> labels <- c(rep(0,12500),rep(1,12500))
+> reviews.all
